4833ff43f0566f4a2f68b6b0425e282685316183
abdwhidd <abdulwahidkaharr@gmail.com>
2024-09-05 02:07:59 +0700

    create user repository

---
+++ repository/UserRepository.php
@@ -0,0 +1,32 @@
+<?php 
+
+class UserRepository {
+    private $conn;
+
+    public function __construct($conn) 
+    {
+        $this->conn = $conn;
+    }
+
+    public function findById($id) 
+    {
+         // Query untuk mencari pengguna berdasarkan ID
+        $query = "SELECT * FROM users WHERE id = :id LIMIT 1";
+        $stmt = $this->conn->prepare($query);
+        
+        // Bind parameter ID ke query
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->execute();
+        
+        // Mengambil hasil query
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        // Jika pengguna ditemukan, membuat objek dan kembalikan objek User
+        if ($row) {
+            return new User($row['id'], $row['name'], $row['email'], $row['password']);
+        }
+        
+        // Jika tidak ditemukan, kembalikan null
+        return null;
+    }
+}
